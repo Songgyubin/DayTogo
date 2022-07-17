e8500a1fd640c10d529c333afa113151db625f8c
songgyubin <thd0427__@naver.com>
2022-07-17 21:50:13 +0900

    ADD LoginActivity

---
+++ app/src/main/java/gyul/songgyubin/daytogo/activities/LoginActivity.kt
@@ -0,0 +1,14 @@
+package gyul.songgyubin.daytogo.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import gyul.songgyubin.daytogo.R
+
+class LoginActivity :AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+    }
+}
