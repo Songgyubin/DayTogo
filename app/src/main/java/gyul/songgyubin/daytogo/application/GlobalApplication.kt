81955cefb2d13165dbc7ac939b90cc685aa51fed
songgyubin <thd0427__@naver.com>
2022-07-25 23:12:00 +0900

    Set naverMapSDK

---
+++ app/src/main/java/gyul/songgyubin/daytogo/application/GlobalApplication.kt
@@ -0,0 +1,15 @@
+package gyul.songgyubin.daytogo.application
+
+import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
+import com.naver.maps.map.NaverMapSdk
+import gyul.songgyubin.daytogo.BuildConfig
+
+
+class GlobalApplication : Application(){
+    override fun onCreate() {
+        super.onCreate()
+        KakaoSdk.init(this,BuildConfig.KakaoSdkKey)
+        NaverMapSdk.getInstance(this).client = NaverMapSdk.NaverCloudPlatformClient(BuildConfig.NaverMapClientId)
+    }
+}
