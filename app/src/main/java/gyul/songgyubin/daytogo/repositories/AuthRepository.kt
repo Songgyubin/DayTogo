87dc8d9f39b663f7353e54d70a93279e01aaa7d9
songgyubin <thd0427__@naver.com>
2022-08-07 22:49:59 +0900

    CLEAN import

--- app/src/main/java/gyul/songgyubin/daytogo/repositories/AuthRepository.kt
+++ app/src/main/java/gyul/songgyubin/daytogo/repositories/AuthRepository.kt
@@ -3,10 +3,7 @@ package gyul.songgyubin.daytogo.repositories
 import android.content.Context
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 import durdinapps.rxfirebase2.RxFirebaseAuth
-import gyul.songgyubin.daytogo.models.User
 import io.reactivex.Maybe
 
 class AuthRepository(context: Context) {
