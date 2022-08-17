04e841741e842936c60d6e5aa65d6d073cabd7e4
songgyubin <thd0427__@naver.com>
2022-08-17 16:03:24 +0900

    REFACT user class

--- app/src/main/java/gyul/songgyubin/daytogo/models/User.kt
+++ app/src/main/java/gyul/songgyubin/daytogo/models/User.kt
@@ -1,14 +1,7 @@
 package gyul.songgyubin.daytogo.models
 
-import com.naver.maps.geometry.LatLng
-import gyul.songgyubin.daytogo.utils.Category
-import java.io.Serializable
-
 
 data class User(
-    val uid: String,
-    val email: String,
-    val locationList: MutableList<Location> = mutableListOf(Location())
-):Serializable
-//t@n.com
-//000000
+    val uid: String="",
+    val email: String="",
+)
