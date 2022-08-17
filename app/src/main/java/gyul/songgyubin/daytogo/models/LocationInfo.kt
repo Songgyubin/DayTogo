0c1fcc6de2288b45b1e81facc6663e469cecdf41
songgyubin <thd0427__@naver.com>
2022-08-17 16:03:46 +0900

    REFACT locationInfo

---
+++ app/src/main/java/gyul/songgyubin/daytogo/models/LocationInfo.kt
@@ -0,0 +1,8 @@
+package gyul.songgyubin.daytogo.models
+
+data class LocationInfo(
+    var title: String = "",
+    var description: String = "",
+    var latitude: Double = 0.0,
+    var longitude: Double = 0.0
+)
