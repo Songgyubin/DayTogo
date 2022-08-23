f739f4b4e4533511a8309cbc6b62589b358a1cd6
songgyubin <thd0427__@naver.com>
2022-08-23 21:11:02 +0900

    Hilt 적용 중

--- presentation/src/main/java/gyul/songgyubin/daytogo/GlobalApplication.kt
+++ presentation/src/main/java/gyul/songgyubin/daytogo/GlobalApplication.kt
@@ -3,9 +3,10 @@ package gyul.songgyubin.daytogo
 import android.app.Application
 import com.kakao.sdk.common.KakaoSdk
 import com.naver.maps.map.NaverMapSdk
+import dagger.hilt.android.HiltAndroidApp
 import gyul.songgyubin.daytogo.BuildConfig
 
-
+@HiltAndroidApp
 class GlobalApplication : Application(){
     override fun onCreate() {
         super.onCreate()
