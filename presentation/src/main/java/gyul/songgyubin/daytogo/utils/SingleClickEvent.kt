683ce1a858e47d230c9357f4ddf4e2462e8cd5de
songgyubin <thd0427__@naver.com>
2022-08-24 16:32:45 +0900

    주석 추가

--- presentation/src/main/java/gyul/songgyubin/daytogo/utils/SingleClickEvent.kt
+++ presentation/src/main/java/gyul/songgyubin/daytogo/utils/SingleClickEvent.kt
@@ -14,6 +14,9 @@ open class SingleClickEvent<event : SingleClickEventFlag>(private val content: S
 
     /**
      * Returns the content and prevents its use again.
+     * 처음 클릭 시 받아온 SingleClickEventFlag를 리턴해주고
+     * MIN_CLICK_INTERVAL안에 재클릭한다면 NONE을 리턴하여
+     * 아무런 동작을 일으키지 않도록한다.
      */
     fun getContentIfNotHandled(): SingleClickEventFlag {
 
