a627e9ae3723e8cf863a73b42320ad24248a9d66
songgyubin <thd0427__@naver.com>
2022-08-24 13:35:43 +0900

    REFACT mapper

--- data/src/main/java/gyul/songgyubin/data/mapper/UserMapper.kt
+++ data/src/main/java/gyul/songgyubin/data/mapper/UserMapper.kt
@@ -4,10 +4,10 @@ import com.google.firebase.auth.FirebaseUser
 import gyul.songgyubin.domain.model.User
 
 object UserMapper {
-    fun mapperToUser(firebaseUser: FirebaseUser): User = User().also {
-        it.uid = firebaseUser.uid
-        it.email = firebaseUser.email!!
-    }
+    fun mapperToUser(firebaseUser: FirebaseUser): User = User(
+        uid = firebaseUser.uid,
+        email = firebaseUser.email!!
+    )
 
 
 }
