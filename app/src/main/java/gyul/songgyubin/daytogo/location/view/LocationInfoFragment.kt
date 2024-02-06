084250f90902e7f22240ddbea7045b955242102d
songgyubin <thd0427__@naver.com>
2024-02-06 23:52:19 +0900

    🚚 패키지 변경

--- app/src/main/java/gyul/songgyubin/daytogo/location/view/LocationInfoFragment.kt
+++ app/src/main/java/gyul/songgyubin/daytogo/location/view/LocationInfoFragment.kt
@@ -1,9 +1,7 @@
-package gyul.songgyubin.daytogo.auth.view
+package gyul.songgyubin.daytogo.location.view
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
