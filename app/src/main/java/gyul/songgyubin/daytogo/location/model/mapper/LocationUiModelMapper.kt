b1a3660b3901c561152b5b40f663f08adfa981af
songgyubin <thd0427__@naver.com>
2024-02-07 10:42:21 +0900

    🏗️ Ui Model Mapping

---
+++ app/src/main/java/gyul/songgyubin/daytogo/location/model/mapper/LocationUiModelMapper.kt
@@ -0,0 +1,38 @@
+package gyul.songgyubin.daytogo.location.model.mapper
+
+import gyul.songgyubin.daytogo.location.model.LocationUiModel
+import gyul.songgyubin.daytogo.utils.extensions.orDefault
+import gyul.songgyubin.domain.location.model.LocationEntity
+
+/**
+ * Location Ui Model와 관련된 Mapper
+ *
+ * @author   Gyub
+ * @created  2024/02/07
+ */
+object LocationUiModelMapper {
+
+    /**
+     * Mapper
+     * List<[LocationEntity]> to List<[LocationUiModel]>
+     */
+    fun List<LocationEntity>.toUiModelList(): List<LocationUiModel> {
+        return this.map {
+            it.toUiModel()
+        }
+    }
+
+    /**
+     * Mapper
+     * [LocationEntity] to [LocationUiModel]
+     */
+    fun LocationEntity.toUiModel(): LocationUiModel {
+        return LocationUiModel(
+            locationId = this.locationId.orEmpty(),
+            title = this.title.orEmpty(),
+            description = this.description.orEmpty(),
+            lat = this.lat.orDefault(),
+            lon = this.lon.orDefault()
+        )
+    }
+}
