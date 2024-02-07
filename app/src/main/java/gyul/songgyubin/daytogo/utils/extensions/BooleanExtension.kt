fabcb77897560913ba83a47e9ba1c5d314cc41e3
songgyubin <thd0427__@naver.com>
2024-02-07 10:09:23 +0900

    🧑‍💻 Primitive 타입 null 처리 확장함수 생성

---
+++ app/src/main/java/gyul/songgyubin/daytogo/utils/extensions/BooleanExtension.kt
@@ -0,0 +1,13 @@
+package gyul.songgyubin.daytogo.utils.extensions
+
+/**
+ * Boolean 형 확장함수
+ *
+ * @author   Gyub
+ * @created  2024/02/07
+ */
+
+/**
+ * null이면 false 반환
+ */
+fun Boolean?.orFalse() = this ?: false
