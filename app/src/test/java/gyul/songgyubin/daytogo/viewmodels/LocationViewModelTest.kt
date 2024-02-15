cf29afa14dfc087952aadae6d1a5cf5e7928ccde
songgyubin <thd0427__@naver.com>
2024-02-15 17:12:41 +0900

    ✅ 저장된 장소 fetch 테스트 코드 추가

--- app/src/test/java/gyul/songgyubin/daytogo/viewmodels/LocationViewModelTest.kt
+++ app/src/test/java/gyul/songgyubin/daytogo/viewmodels/LocationViewModelTest.kt
@@ -1,11 +1,13 @@
 package gyul.songgyubin.daytogo.viewmodels
 
+import com.google.firebase.auth.FirebaseAuth
 import gyul.songgyubin.daytogo.location.model.LocationUiModel
 import gyul.songgyubin.daytogo.location.viewmodel.LocationViewModel
 import gyul.songgyubin.domain.location.model.LocationEntity
 import gyul.songgyubin.domain.location.usecase.AddLocationInfoUseCase
 import gyul.songgyubin.domain.location.usecase.GetRemoteSavedLocationInfoUseCase
 import io.mockk.coEvery
+import io.mockk.every
 import io.mockk.mockk
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -14,10 +16,13 @@ import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runBlockingTest
 import kotlinx.coroutines.test.setMain
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
+import org.mockito.Mockito.verify
+import org.mockito.Mockito.`when`
 
 /**
  * Location View Model Test
@@ -29,15 +34,19 @@ import org.junit.Test
 class LocationViewModelTest {
 
     private lateinit var viewModel: LocationViewModel
+
+    private val auth: FirebaseAuth = mockk()
+
     private val addLocationInfoUseCase: AddLocationInfoUseCase = mockk()
     private val getRemoteSavedLocationInfoUseCase: GetRemoteSavedLocationInfoUseCase = mockk()
+
 
     private val testDispatcher = StandardTestDispatcher()
 
     @Before
     fun setUp() {
         Dispatchers.setMain(testDispatcher)
-        viewModel = LocationViewModel(addLocationInfoUseCase, getRemoteSavedLocationInfoUseCase)
+        viewModel = LocationViewModel(auth, addLocationInfoUseCase, getRemoteSavedLocationInfoUseCase)
     }
 
     @After
@@ -67,5 +76,18 @@ class LocationViewModelTest {
 
         viewModel.setSavedLocationInfo(locationUiModel)
         assert(viewModel.savedLocationUiModel["1"] == locationUiModel)
+    }
+
+    @Test
+    fun `fetchSavedLocationList fetches data and updates state`() = runBlocking {
+        val fakeUserId = "user123"
+        val fakeLocationList = listOf(LocationEntity("1", "Home", null, 0.0, 0.0))
+
+        every { auth.currentUser?.uid } returns fakeUserId
+        every { getRemoteSavedLocationInfoUseCase(fakeUserId) } returns flowOf(fakeLocationList)
+
+        viewModel.fetchSavedLocationList()
+
+        assert(viewModel.savedLocationList.value == fakeLocationList)
     }
 }
